ccd6adb7513c30886607bb45e10d1c886f7dde57
KatherinaKaplina <156842594+KatherinaKaplina@users.noreply.github.com>
2024-01-28 21:03:52 +0200

    Add Task1

---
+++ Tasks/Kaplina/Task1/Task1_4.py
@@ -0,0 +1,11 @@
+# 4.Write a program that converts some amount of money from USD to BYN, ask a user for the amount, store the ratio inside the program itself.
+
+import math
+
+money_USD = int(input("Please, enter the amount of money in USD\n"))
+money_BYN = money_USD * 3.29
+print("It's", money_BYN, "BYN")
+
+
+
+
