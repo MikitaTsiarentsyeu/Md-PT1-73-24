eed389ac593f5e132bee1a70d6ab65cfc7a2937d
Olga Blashko <126601252+Helga-bc@users.noreply.github.com>
2024-03-04 19:01:33 +0400

    Create Task6

---
+++ Tasks/Blashko/Task6/Task6_4.py
@@ -0,0 +1,13 @@
+'''4. Write a recursive function to calculate the power of a given number.'''
+
+def recursive_power(n:int, power:int):
+    if power == 1:
+        return n
+    return n * recursive_power(n, power-1) 
+    
+
+    
+n = int(input('Enter a number:\n'))
+p = int(input('To what power should we raise the number?:\n'))
+    
+print(f'{n} in power {p} is {recursive_power(n, p)}')
