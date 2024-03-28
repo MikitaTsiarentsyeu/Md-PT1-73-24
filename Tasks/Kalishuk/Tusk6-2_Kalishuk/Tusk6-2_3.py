884342f3aa9c8607030725e446fa694071b63818
Dzmitry <kalishuk5567@gmail.com>
2024-03-28 18:03:18 +0300

    creat my new Tusk

---
+++ Tasks/Kalishuk/Tusk6-2_Kalishuk/Tusk6-2_3.py
@@ -0,0 +1,19 @@
+def find_count(string:str, symb:str):
+    count = 0
+    if len(string) == 1:
+        if string == symb:
+            return 1
+        else:
+            return 0
+    else:
+        if string[0] == symb:
+            count = 1 + find_count(string[1:], symb)
+        else:
+            count = 0 + find_count(string[1:], symb)
+        return count
+        
+    
+s = input('Enter a string:\n') 
+symb = input('Enter a symbol that you want to count:\n')
+    
+print(find_count(s, symb))
