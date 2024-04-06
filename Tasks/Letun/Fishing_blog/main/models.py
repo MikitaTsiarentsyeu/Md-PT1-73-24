277cd12490956eeb2cf30e032790aeef05648031
Yan <burning43arrow@gmail.com>
2024-04-06 14:15:30 +0300

    Creating models and setting up the admin panel

--- Tasks/Letun/Fishing_blog/main/models.py
+++ Tasks/Letun/Fishing_blog/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    name = models.CharField(blank=False, max_length=100)
+    email = models.EmailField(primary_key=True)
+
+class Post(models.Model):
+
+    POST_TYPES = [('c', "copyright"), ('a', "marketing")]
+
+    title = models.CharField(max_length=100)    
+    content = models.TextField()
+    issued = models.DateTimeField()
+    post_type = models.CharField(choices=POST_TYPES, max_length=1)
+    image = models.ImageField(upload_to='uploads')
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
