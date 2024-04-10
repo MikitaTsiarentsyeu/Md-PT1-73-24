0872ae56d4e2b407a95527691c0a61e5d930385d
Yan <burning43arrow@gmail.com>
2024-04-10 11:18:38 +0300

    Creating and connecting urls

--- Tasks/Letun/Task3/task3_9.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(input_string):
-    reversed_string = ""
-    for char in reversed(input_string):
-        reversed_string += char
-    return reversed_string
-
-user_input = input("Введите строку:")
-reversed_input = reverse_string(user_input)
-print("Строка в обратном порядке:", reversed_input)
