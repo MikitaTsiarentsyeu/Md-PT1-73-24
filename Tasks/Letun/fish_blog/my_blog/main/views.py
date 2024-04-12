1fc2de5ee7b1fcc63d5374bc2f4ccc8dd687057e
Yan <burning43arrow@gmail.com>
2024-04-12 19:36:11 +0300

    Add a switch for a template detail.html

--- Tasks/Letun/fish_blog/my_blog/main/views.py
+++ Tasks/Letun/fish_blog/my_blog/main/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
@@ -21,9 +24,18 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("main/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "main/index.html", context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "main/detail.html", {"question": question})
     
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})   
