d878d81ec3e4e51a1b5023503d42969beb4f3973
Yan <burning43arrow@gmail.com>
2024-04-14 14:34:36 +0300

    Working with a directory for static files

--- Tasks/Letun/fish_blog/my_blog/main/views.py
+++ Tasks/Letun/fish_blog/my_blog/main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 
@@ -28,14 +28,24 @@ class IndexView(generic.ListView):
     template_name = "main/index.html"
     context_object_name = "latest_question_list"
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
-
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+    :5
+    ]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "main/detail.html"
+      
+def get_queryset(self):
+    """
+    Excludes any questions that aren't published yet.
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -50,12 +60,12 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
 
-        return render(
+      return render(
             request,
             "main/detail.html",
             {
-                "question": question,
-                "error_message": "You didn't select a choice.",
+            "question": question,
+            "error_message": "You didn't select a choice.",
             },
         )
     else:
