3a9cf38f8810dec7569f8808f289fbe280b494d3
Yan <burning43arrow@gmail.com>
2024-04-15 12:46:33 +0300

    Update the index view to use the main/views.py template

--- Tasks/Letun/fish_blog/my_blog/main/views.py
+++ Tasks/Letun/fish_blog/my_blog/main/views.py
@@ -7,9 +7,16 @@ from django.views import generic
 from django.utils import timezone
 from .models import Choice, Question
 
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("main/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
